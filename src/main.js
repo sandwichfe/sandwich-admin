100d234cafe5e4ca499717551edfa81ec0eeebf7
sandwichfe <1344382941@qq.com>
2023-08-07 14:15:41 +0800

    postcss自适应插件相关配置

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './util/rem'
 
 // element-ui
 import ElementUI from 'element-ui';
